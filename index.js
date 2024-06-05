445ed554992c8ad9877a25e18e577408b37a1797
Najma Javed <najma.javed@outlook.com>
2024-06-05 12:03:46 +0200

    Lab03- JS datatype solved

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -21,11 +21,12 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const result = `${part1.slice(0, -1) + part1.slice(-1).toUpperCase()}${part2.slice(0, -1) + part2.slice(-1).toUpperCase()}`;
 
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +36,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * (15/100);
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,11 +48,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() *10) +1;
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +63,25 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+// fasle
 const expression2 = a || b;
-
+// true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
+
+//print to console
+console.log(`${expression1}
+${expression2}
+${expression3}
+${expression4}
+${expression5}
+${expression6}
+${expression7}`);
